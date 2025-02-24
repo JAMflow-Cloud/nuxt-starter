8f313cc03879afc23f1dc40079ba9efeda99f69f
Sandro Circi <sandro.circi@digitoolmedia.com>
2025-02-24 12:38:24 +0100

    feat(v4): add Nuxt UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
+    '@nuxt/ui'
   ],
 
   devtools: { enabled: true },
+
+  css: [
+    'assets/css/main.css',
+  ],
 
   future: {
     compatibilityVersion: 4,
